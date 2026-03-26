5e4ef27d08f1234baff3101fbffc08536ea1ca43
Andres lopez <anderslogo@gmail.com>
2026-03-26 14:03:31 -0500

    Fix CSP para permitir onclick en pestañas

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "https://cdnjs.cloudflare.com", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],  // 
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "https://media.presidencia.gov.co", "data:"],
